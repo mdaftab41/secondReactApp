3d77004a5cc942c556e7522c04d04c8e160e8a7d
mdaftab41 <125267632+mdaftab41@users.noreply.github.com>
2023-08-24 01:45:18 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite food</h1>
+    <ul>
+      <li>Banana</li>
+      <li>Mango</li>
+      <li>Orange</li>
+    </ul>
+    <h2>Favourite Game </h2>
+    <ol>
+      <li>Cricket</li>
+      <li>Football</li>
+      <li>Baskitball</li>
+    </ol>
+  </div>,
+  document.getElementById("root")
+);
